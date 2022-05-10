9d4a8caf11534da44feff588b7996495cfa5c2be
Nadina Amsler <nadina.amsler@icloud.com>
2022-05-10 09:08:07 +0200

    appliaktionerstellen

---
+++ bookshelf/src/main/java/ch/bzz/bookshelf/model/Publisher.java
@@ -0,0 +1,34 @@
+package ch.bzz.bookshelf.model;
+
+import java.util.List;
+
+public class Publisher {
+
+    private String publisherUUID;
+    private String publisher;
+    public List<Book> booksList;
+
+    public String getPublisherUUID() {
+        return publisherUUID;
+    }
+
+    public void setPublisherUUID(String publisherUUID) {
+        this.publisherUUID = publisherUUID;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public List<Book> getBooksList() {
+        return booksList;
+    }
+
+    public void setBooksList(List<Book> booksList) {
+        this.booksList = booksList;
+    }
+}
